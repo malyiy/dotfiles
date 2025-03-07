3d6b70a5d564929e771dd28d8b9e4c966c0f1d96
Kirill Beznisko <beznisko.k@proton.me>
2025-03-07 17:05:39 +0200

    Update

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -75,7 +75,7 @@
 				# ];
 
 				# Auto upgrade nix package and the daemon service.
-				services.nix-daemon.enable = true;
+				nix.enable = true;
 				# nix.package = pkgs.nix;
 
 				# Necessary for using flakes on this system.
